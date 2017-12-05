1daa027e0695462190d04bc9517bfdf98108e6f6
Luke Stanley <lukewgs@gmail.com>
2017-12-05 10:37:48 -0600

    completed day 5

---
+++ src/day5-a.py
@@ -0,0 +1,28 @@
+DAY = '5'
+PUZ = 'a'
+infile_name = "./dat/day" + DAY + "-" + 'a' + "-in.dat"
+outfile_name = "./res/day" + DAY + "-" + PUZ + "-out.dat"
+
+infile = open(infile_name, 'r')
+#BEGIN:
+instructions = []
+
+for num in infile.read()[:-1].split('\n'):
+	instructions.append(int(num))
+
+location = 0
+steps = 0
+while location >= 0 and location < len(instructions):
+	next_location = location + instructions[location]
+	
+	instructions[location] += 1
+	
+	location = next_location
+	steps += 1
+
+result = steps
+
+infile.close()
+outfile = open(outfile_name, 'w')
+outfile.write(str(result))
+outfile.close()
